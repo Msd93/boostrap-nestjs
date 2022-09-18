8f7a12dd57931efe019fc99fcdb465cb5ed2ad14
Mamadou seydou DIALLO <diallodiery301gmail.com>
2022-09-18 16:58:04 +0000

    integration prismix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './config/db/prisma.service';
+import { PostService } from './modules/shared/services/post.service';
+import { UserService } from './modules/shared/services/user.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [UserService, PostService, PrismaService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './config/db/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // NestJS config
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app)
+
   await app.listen(3000);
 }
 bootstrap();
